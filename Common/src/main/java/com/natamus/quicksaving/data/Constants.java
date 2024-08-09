51ca34e12c3fe362a197f057fc8275cfbfce5ff5
Rick South <rick@natamus.com>
2024-08-09 08:10:11 +0200

    Quicksaving source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/quicksaving/data/Constants.java
@@ -0,0 +1,11 @@
+package com.natamus.quicksaving.data;
+
+import net.minecraft.client.KeyMapping;
+
+public class Constants {
+    public static KeyMapping quicksavingKey;
+    public static KeyMapping quickloadKey;
+
+    public static final int F6key = 295;
+    public static final int F8key = 297;
+}
