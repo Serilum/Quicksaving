755536092b37415ff58de0cb2eb5b0ccc09e0f59
Rick South <rick@natamus.com>
2025-10-03 01:14:02 +0200

    (v1.6) Updated mod to use Collective's new RegisterKeyMappingsHelper.

--- Common/src/main/java/com/natamus/quicksaving/ModCommon.java
+++ Common/src/main/java/com/natamus/quicksaving/ModCommon.java
@@ -1,6 +1,8 @@
 package com.natamus.quicksaving;
 
+import com.natamus.collective.services.Services;
 import com.natamus.quicksaving.config.ConfigHandler;
+import com.natamus.quicksaving.data.Constants;
 import com.natamus.quicksaving.networking.PacketRegistration;
 
 public class ModCommon {
@@ -19,5 +21,10 @@ public class ModCommon {
 
 	public static void registerPackets() {
 		new PacketRegistration().init();
+	}
+
+	public static void registerHotkeys() {
+		Constants.quicksavingKey = Services.REGISTERKEYMAPPING.registerKeyMapping("quicksaving.key.quicksaving", Constants.F6key, "quicksaving.key.quicksaving");
+		Constants.quickloadKey = Services.REGISTERKEYMAPPING.registerKeyMapping("quicksaving.key.quickload", Constants.F8key, "quicksaving.key.quicksaving");
 	}
 }
