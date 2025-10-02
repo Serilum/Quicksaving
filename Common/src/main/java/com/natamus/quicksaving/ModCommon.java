1d9ca355c5c32d51c4156c122653811ec53edc35
Rick South <rick@natamus.com>
2025-10-03 01:14:10 +0200

    Quicksaving source code for 1.21.9.

---
+++ Common/src/main/java/com/natamus/quicksaving/ModCommon.java
@@ -0,0 +1,35 @@
+package com.natamus.quicksaving;
+
+import com.natamus.collective.services.Services;
+import com.natamus.quicksaving.config.ConfigHandler;
+import com.natamus.quicksaving.data.Constants;
+import com.natamus.quicksaving.networking.PacketRegistration;
+import com.natamus.quicksaving.util.Reference;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+
+		registerPackets();
+
+		load();
+	}
+
+	private static void load() {
+
+	}
+
+	public static void registerPackets() {
+		new PacketRegistration().init();
+	}
+
+	public static void registerHotkeys() {
+		KeyMapping.Category keyMappingsCategory = KeyMapping.Category.register(ResourceLocation.fromNamespaceAndPath(Reference.MOD_ID, "quicksaving"));
+
+		Constants.quicksavingKey = Services.REGISTERKEYMAPPING.registerKeyMapping("quicksaving.key.quicksaving", Constants.F6key, keyMappingsCategory);
+		Constants.quickloadKey = Services.REGISTERKEYMAPPING.registerKeyMapping("quicksaving.key.quickload", Constants.F8key, keyMappingsCategory);
+	}
+}
---
+++ Forge/src/main/java/com/natamus/quicksaving/ModForge.java
@@ -0,0 +1,45 @@
+package com.natamus.quicksaving;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.quicksaving.forge.config.IntegrateForgeConfig;
+import com.natamus.quicksaving.forge.events.ForgeQuicksaveEvents;
+import com.natamus.quicksaving.util.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+		ModCommon.registerHotkeys();
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(modLoadingContext);
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		if (FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			ForgeQuicksaveEvents.registerEventsInBus();
+		}
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
