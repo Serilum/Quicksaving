5d5b91bd21b33752e83a0be1c946ab461c06e02c
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:32:15 +0200

    (v1.8) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/quicksaving/networking/packets/ToServerTeleportPlayerPacket.java
+++ Common/src/main/java/com/natamus/quicksaving/networking/packets/ToServerTeleportPlayerPacket.java
@@ -47,7 +47,7 @@ public class ToServerTeleportPlayerPacket {
             Player player = ctx.sender();
 
 			if (ConfigHandler.musthaveCheatAccessForQuickloadOnServer && !player.hasPermissions(2)) {
-				MessageFunctions.sendMessage(player, "With the current server configuration, you may only quickload with cheat access enabled.", ChatFormatting.RED);
+				MessageFunctions.sendTranslatableMessage(player, "collective.quicksaving.message.currentserverconfiguration", ChatFormatting.RED);
 				return;
 			}
 
@@ -62,7 +62,7 @@ public class ToServerTeleportPlayerPacket {
                 Services.TELEPORT.teleportEntity(player, packet.teleportDimension, packet.teleportLocation);
             }
 
-            player.displayClientMessage(Component.literal("Quickloaded.").withStyle(ChatFormatting.DARK_GREEN), true);
+            player.displayClientMessage(Component.translatable("collective.quicksaving.message.quickloaded").withStyle(ChatFormatting.DARK_GREEN), true);
         }
     }
 }
